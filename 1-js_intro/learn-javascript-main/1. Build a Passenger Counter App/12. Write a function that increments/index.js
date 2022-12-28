e16ab9c39d2535604f5b6aa1cb24b7747efe06f1
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-28 15:23:15 +0100

    add

---
+++ 1-js_intro/learn-javascript-main/1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -0,0 +1,14 @@
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementLapsCompleted() {
+    lapsCompleted += 1;
+}
+
+incrementLapsCompleted();
+incrementLapsCompleted();
+incrementLapsCompleted();
+
+console.log(lapsCompleted)
---
+++ 1-js_intro/learn-javascript-main/1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count").innerText = 5
+
+// cmd+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+let myAge, humanDogRatio, myDogAge;
+
+myAge = 20;
+humanDogRatio = 7;
+myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
+
