53e46fcf7ad684cda14e2baaedbc05731be13d68
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 17:11:42 +0100

    Adding a new format

--- 1-js_intro/2. Practice time - part 1/1. Variables practice/index.js
+++ 1-js_intro/2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,8 @@
 // Create two variables, firstName and lastName
-
+let firstName = Hayatudeen;
+let lastName = Abdulrahman;
 // Concatenate the two variables into a third variable called fullName
-
+let fullName =
 // Log fullName to the console
 
 
