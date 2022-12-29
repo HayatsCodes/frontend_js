1395f6c5ee2db1801e64f8809b7ba5487e9d43b3
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 17:12:42 +0100

    Refactoring the code

--- 1-js_intro/2. Practice time - part 1/1. Variables practice/index.js
+++ 1-js_intro/2. Practice time - part 1/1. Variables practice/index.js
@@ -1,8 +1,9 @@
 // Create two variables, firstName and lastName
-let firstName = Hayatudeen;
-let lastName = Abdulrahman;
+let firstName = "Hayatudeen";
+let lastName = "Abdulrahman";
 // Concatenate the two variables into a third variable called fullName
-let fullName =
+let fullName = firstName + " " + lastName;
 // Log fullName to the console
+console.log(fullName);
 
 
