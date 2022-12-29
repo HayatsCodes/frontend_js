046a23c8006e68e5239e556d94921ba2f6d47d92
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 17:40:56 +0100

    Improving the build process

--- 1-js_intro/2. Practice time - part 1/6. Calculator challenge/index.js
+++ 1-js_intro/2. Practice time - part 1/6. Calculator challenge/index.js
@@ -1,29 +1,29 @@
-let num1 = 8
-let num2 = 2
-let result = 0
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = 8;
+let num2 = 2;
+let result = 0;
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
 
 // Create four functions: add(), subtract(), divide(), multiply()
 function add() {
-    result = num1 + num2
+    result = num1 + num2;
 }
 
 function subtract() {
-    result = num1 - num2
+    result = num1 - num2;
 }
 
 function divide() {
-    result = num1 / num2
+    result = num1 / num2;
 }
 function add() {
-    result = num1 * num2
+    result = num1 * num2;
 }
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 let sumEl = document.getElementById("sum-el");
-sumEl.innerText = 
+sumEl.innerText = result;
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
