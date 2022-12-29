2963c27efaa05ad93f069f2b30894746e9abfffd
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 18:07:07 +0100

    Updating the infrastructure

--- 1-js_intro/3. Build a Blackjack game/6. The if...else statement for our game/index.js
+++ 1-js_intro/3. Build a Blackjack game/6. The if...else statement for our game/index.js
@@ -5,6 +5,7 @@ let sum = firstCard + secondCard
 // Write the conditional according to these rules:
 
 // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+if (sum )
 // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
 // else -> "You're out of the game! 😭"
 
