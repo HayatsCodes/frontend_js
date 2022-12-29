70771ce4493137c7acc4e2cf4d3f97f07e2db088
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 16:37:32 +0100

    Updating the codebase

--- 1-js_intro/1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ 1-js_intro/1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,5 +1,5 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
-
+let welcomeEl = document.getElementById()
 // Create two variables (name & greeting) that contains your name
 // and the greeting we want to render on the page
 
