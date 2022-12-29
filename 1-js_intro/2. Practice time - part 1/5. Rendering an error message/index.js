5299cf4e48fa725c1b7dc1d8d39b7d0eb62cd253
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 17:25:49 +0100

    Adding a new format

--- 1-js_intro/2. Practice time - part 1/5. Rendering an error message/index.js
+++ 1-js_intro/2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,6 +1,7 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
-let errorMsg = document.getElementById()
+let errorMsg = document.getElementById("error")
+
 
 
