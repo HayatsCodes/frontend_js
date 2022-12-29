ff87ac9806aed6cd4a6df49db6ae9ac16d3796db
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 16:38:33 +0100

    Adding a new format

--- 1-js_intro/1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ 1-js_intro/1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,8 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById()
+let welcomeEl = document.getElementById("welcome-el");
 // Create two variables (name & greeting) that contains your name
+let myName = "Hayatudeen Abdulrahman";
+let 
 // and the greeting we want to render on the page
 
 // Render the welcome message using welcomeEl.innerText
