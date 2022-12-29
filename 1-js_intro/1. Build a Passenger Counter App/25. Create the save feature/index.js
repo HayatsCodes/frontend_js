eac8f13a6b182ef406efc91f85986ccdcf4035da
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 16:54:37 +0100

    Removing unused code

--- 1-js_intro/1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1-js_intro/1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -11,9 +11,9 @@ function increment() {
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    let saveCount = count + " -";
+    let saveCount = count + " - ";
     // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += 
+    saveEl.innerText += saveCount;
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
