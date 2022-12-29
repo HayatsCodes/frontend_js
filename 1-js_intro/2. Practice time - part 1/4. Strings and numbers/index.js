e51d45bcfe14c6b145eecc98f10c401317a85cc3
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 17:20:47 +0100

    Adding a new page

--- 1-js_intro/2. Practice time - part 1/4. Strings and numbers/index.js
+++ 1-js_intro/2. Practice time - part 1/4. Strings and numbers/index.js
@@ -1,6 +1,6 @@
 // Try to predict what each of the lines will log out
-console.log("2" + 2) //
-console.log(11 + 7) //
+console.log("2" + 2) // 22
+console.log(11 + 7) // 
 console.log(6 + "5") //
 console.log("My points: " + 5 + 9) //
 console.log(2 + 2) // 
