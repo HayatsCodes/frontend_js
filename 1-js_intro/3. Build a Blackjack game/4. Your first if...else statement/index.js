0c7f15f6b886faad4ed4eb1beb3724c54bbf445a
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-29 18:02:05 +0100

    Updating the codebase

--- 1-js_intro/3. Build a Blackjack game/4. Your first if...else statement/index.js
+++ 1-js_intro/3. Build a Blackjack game/4. Your first if...else statement/index.js
@@ -1,14 +1,14 @@
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
 
-let age = 22
+let age = 22;
 
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
 if (age < 22) {
-    console.log("You're not elligible for a driver license")
+    console.log("You're not elligible for a driver license");
 }
 else {
-    console.log()
+    console.log("Welcome!");
 }
 
