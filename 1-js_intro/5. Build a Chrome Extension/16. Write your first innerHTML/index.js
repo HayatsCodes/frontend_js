d22df2ff9ee0aae078459328ef34ac7fc3e1831c
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 16:38:13 +0100

    Enhancing the code style

--- 1-js_intro/5. Build a Chrome Extension/16. Write your first innerHTML/index.js
+++ 1-js_intro/5. Build a Chrome Extension/16. Write your first innerHTML/index.js
@@ -1,4 +1,8 @@
 // Use .innerHTML to render a Buy! button inside the div container
 
 const container = document.getElementById("container")
-container.innerHTML = "<button onclick=">" + "Buy" + "</button>"
+container.innerHTML = "<button onclick='buy()'>" + "Buy" + "</button>"
+
+function buy() {
+    console.log("Buy!")
+}
