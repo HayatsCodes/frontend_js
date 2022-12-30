8134339512e17ab97d90ced9fcd8bef5415cb76e
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 11:06:47 +0100

    Enhancing the code style

--- 1-js_intro/3. Build a Blackjack game/46. Write your first logical operator/index.js
+++ 1-js_intro/3. Build a Blackjack game/46. Write your first logical operator/index.js
@@ -4,7 +4,9 @@ let hasHintsLeft = false
 // Create an if statement that checks that both variables are false.
 // If so, run the showSolution() function
 
-if (hasSolvedChallenge === false && hasHintsLeft === false )
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution()
+}
 
 function showSolution() {
     console.log("Showing the solution....")
