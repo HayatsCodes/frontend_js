ccb0a19738ca71e86951cc50ffd457922cc63844
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 12:32:32 +0100

    Improving the build process

--- 1-js_intro/3. Build a Blackjack game/51. Create your first object/index.js
+++ 1-js_intro/3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,9 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
-let castleListing = {
+let castle = {
     rooms: 4,
     bathTub: true,
     type: "family"
 }
+
+console.log(castle.rooms)
+console.log(castle.type)
 
