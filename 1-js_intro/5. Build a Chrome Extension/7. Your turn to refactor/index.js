23edc1ced21e8ccde5e2581d523eda500de54a60
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 15:03:37 +0100

    Adding a new page

--- 1-js_intro/5. Build a Chrome Extension/7. Your turn to refactor/index.js
+++ 1-js_intro/5. Build a Chrome Extension/7. Your turn to refactor/index.js
@@ -1,8 +1,8 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-saveLoad = 
-
+saveLoad = document.getElementById("input-btn")
+saveLoad.addEventListener("click", function())
 // function saveLead() {
 //     console.log("Button clicked!")
 // }
