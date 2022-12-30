aa3b3dfd52966a0df029107308a9db264f0fc8ab
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 10:05:16 +0100

    Removing unused code

--- 1-js_intro/3. Build a Blackjack game/32. Use a loop to render cards/index.js
+++ 1-js_intro/3. Build a Blackjack game/32. Use a loop to render cards/index.js
@@ -14,6 +14,7 @@ function startGame() {
 }
 
 function renderGame() {
+    cardsEl.textContent = ""
     // Create a for loop that renders out all the cards instead of just two
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += " " + cards[i] + " " 
