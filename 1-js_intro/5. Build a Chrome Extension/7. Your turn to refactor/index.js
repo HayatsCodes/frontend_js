515906ffa5d607e48c1e3a2bd3344b1c62a76b27
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 15:02:36 +0100

    Updating the infrastructure

--- 1-js_intro/5. Build a Chrome Extension/7. Your turn to refactor/index.js
+++ 1-js_intro/5. Build a Chrome Extension/7. Your turn to refactor/index.js
@@ -1,9 +1,11 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
-    console.log("Button clicked!")
-}
+saveLoad = 
+
+// function saveLead() {
+//     console.log("Button clicked!")
+// }
 
 
 
