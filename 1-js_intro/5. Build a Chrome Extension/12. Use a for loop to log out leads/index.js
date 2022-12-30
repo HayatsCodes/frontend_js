3d2e4004529cbb62fc78bf9ed2441d889c3906c0
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 15:22:44 +0100

    Adding a new option

--- 1-js_intro/5. Build a Chrome Extension/12. Use a for loop to log out leads/index.js
+++ 1-js_intro/5. Build a Chrome Extension/12. Use a for loop to log out leads/index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com"]
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
@@ -8,7 +8,10 @@ inputBtn.addEventListener("click", function() {
 })
 
 
-// Log out the items in the myLeads array using a for loop 
+// Log out the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+    console.log
+}
 
 
 
