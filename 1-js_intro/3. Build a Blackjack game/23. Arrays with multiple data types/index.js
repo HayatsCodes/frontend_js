61b5c4512e5f9ffde0b79eaa206c96535c7aee02
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 09:37:03 +0100

    Adding a new option

--- 1-js_intro/3. Build a Blackjack game/23. Arrays with multiple data types/index.js
+++ 1-js_intro/3. Build a Blackjack game/23. Arrays with multiple data types/index.js
@@ -3,4 +3,4 @@
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
 
-let about
+let aboutMe = ["Hayatudeen Abdulrahman", 20, false]
