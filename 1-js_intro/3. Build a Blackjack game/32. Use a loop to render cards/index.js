d6494a34aec08a4a49afaa1c2c032bbab6f8a6dd
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 10:06:17 +0100

    Fixing typos

--- 1-js_intro/3. Build a Blackjack game/32. Use a loop to render cards/index.js
+++ 1-js_intro/3. Build a Blackjack game/32. Use a loop to render cards/index.js
@@ -14,10 +14,10 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = ""
+    cardsEl.textContent = "Cards: "
     // Create a for loop that renders out all the cards instead of just two
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += " " + cards[i] + " " 
+        cardsEl.textContent += cards[i] + " "
     }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
