2dafe30d5eb4825150c53395939d89e70778fce0
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-30 09:58:13 +0100

    Removing unused code

--- 1-js_intro/3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
+++ 1-js_intro/3. Build a Blackjack game/31. For loops, arrays, and DOM/index.js
@@ -4,5 +4,5 @@ let greetingEl = document.getElementById("greeting-el")
 // Render the sentence in the greetingEl paragraph using a for loop and .textContent
 
 for (let i= 0; i < sentence.length; i++) {
-    greetingEl.textContent += i
+    greetingEl.textContent += sentence[i]
 }
