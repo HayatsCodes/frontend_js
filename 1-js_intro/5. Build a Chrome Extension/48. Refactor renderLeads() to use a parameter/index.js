ad34e8095c33481d522d3ae6cbae9446868b6e52
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 12:08:09 +0100

    Fixing a bug

--- 1-js_intro/5. Build a Chrome Extension/48. Refactor renderLeads() to use a parameter/index.js
+++ 1-js_intro/5. Build a Chrome Extension/48. Refactor renderLeads() to use a parameter/index.js
@@ -7,20 +7,20 @@ const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 // Refector the function so that it takes a parameter, leads, that it uses
 // instead of the global myLeads variable. Remember to update all invocations 
 // of the function as well.
 
-function render() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
