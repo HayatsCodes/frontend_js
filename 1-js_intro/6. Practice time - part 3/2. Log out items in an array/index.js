c318daace702e9f21ef10db2eb20e1b2675bc2d8
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 13:27:43 +0100

    Updating the code

--- 1-js_intro/6. Practice time - part 3/2. Log out items in an array/index.js
+++ 1-js_intro/6. Practice time - part 3/2. Log out items in an array/index.js
@@ -3,6 +3,8 @@ let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Cle
 // Create a function that takes a single parameter, an array,
 // and logs all the items of the array to the console.
 function logArray(array) {
-    
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
 }
 // Call the function while passing in myCourses as an argument
