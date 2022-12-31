5980559dd3aec97ee3aa4d0f207f91eb9b577173
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 09:37:14 +0100

    Updating the code

--- 1-js_intro/5. Build a Chrome Extension/25. Write your first template string/index.js
+++ 1-js_intro/5. Build a Chrome Extension/25. Write your first template string/index.js
@@ -4,6 +4,8 @@ const recipient = "James"
 const sender = "Hayats"
 
 // Refactor the email string to use template strings
-const email = `Hey  ${recipient}! How is it going? Cheers ${sender}`
+const email = `Hey  
+    ${recipient}! How is it going? 
+    Cheers ${sender}`
 
 console.log(email)
