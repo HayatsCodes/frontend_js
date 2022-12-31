91805333163d55044b6fe3253c163216e8721f88
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 13:30:46 +0100

    Updating the documentation

--- 1-js_intro/6. Practice time - part 3/3. save to localStorage/index.js
+++ 1-js_intro/6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,4 +1,5 @@
 // Save a value to localStorage
-localStorage.setItem("salary", "$40k/year")
+// localStorage.setItem("salary", "$40k/year")
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+localStorage.getItem()
