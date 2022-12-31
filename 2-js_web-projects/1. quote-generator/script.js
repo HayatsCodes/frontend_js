a1d1002c837aaea8f539633c10d312cca88561a8
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 19:26:03 +0100

    Updating the documentation

--- 2-js_web-projects/1. quote-generator/script.js
+++ 2-js_web-projects/1. quote-generator/script.js
@@ -1,6 +1,9 @@
 let apiQuotes = []
 
-// Show 
+// Show New Quote
+function newQuote() {
+    const quote = apiQuotes[Math]
+}
 // Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
