58c406b8256b82abed301e30e71197dd31b9e2f3
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 19:09:53 +0100

    Adding a new feature

--- 2-js_web-projects/1. quote-generator/script.js
+++ 2-js_web-projects/1. quote-generator/script.js
@@ -2,5 +2,8 @@ let apiQuotes = []
 
 // Get Quotes From API
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api'
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl)
+    }
 }
