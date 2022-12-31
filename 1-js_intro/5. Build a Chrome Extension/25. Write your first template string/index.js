87d4a9932fff30df83c92b33bb13ecde7151ced4
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 09:36:13 +0100

    Fixing a bug

--- 1-js_intro/5. Build a Chrome Extension/25. Write your first template string/index.js
+++ 1-js_intro/5. Build a Chrome Extension/25. Write your first template string/index.js
@@ -1,8 +1,9 @@
 // template strings/literals
 
 const recipient = "James"
+const sender = "Hayats"
 
 // Refactor the email string to use template strings
-const email = `Hey  ${recipient}! How is it going? Cheers Per`
+const email = `Hey  ${recipient}! How is it going? Cheers ${sender}`
 
 console.log(email)
