efa977dafb1ffa3f79a71e77da5b78070599f356
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 13:45:57 +0100

    Adding a new feature

--- 1-js_intro/6. Practice time - part 3/5. Generate sentence/index.js
+++ 1-js_intro/6. Practice time - part 3/5. Generate sentence/index.js
@@ -14,7 +14,10 @@ function generateSentence(desc, arr) {
     // }
     let sentence = `The ${arr.length} ${desc} are `
     for (let i = 0; i < arr.length; i++) {
-        sentence += arr[i] + " "
+        if (i != arr.length - 1) {
+
+        }
+        
     }
     return sentence
 }
