33ee74c5d28dfa3ec741bf638124e005e03bf941
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 11:04:43 +0100

    Refactoring the code

--- 1-js_intro/5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
+++ 1-js_intro/5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 // Get the leads from the localStorage
+localStorage.getItem("")
 // Store it in a variable, leadsFromLocalStorage
 // Log out the variable
 
