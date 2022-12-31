4382ab9d3b6f4474f880cb0a4908e32c02aa8356
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 13:46:58 +0100

    Fixing a bug

--- 1-js_intro/6. Practice time - part 3/5. Generate sentence/index.js
+++ 1-js_intro/6. Practice time - part 3/5. Generate sentence/index.js
@@ -15,7 +15,10 @@ function generateSentence(desc, arr) {
     let sentence = `The ${arr.length} ${desc} are `
     for (let i = 0; i < arr.length; i++) {
         if (i != arr.length - 1) {
-
+            sentence += arr[i] + ", "
+        }
+        else {
+            sentence += arr[i]
         }
         
     }
