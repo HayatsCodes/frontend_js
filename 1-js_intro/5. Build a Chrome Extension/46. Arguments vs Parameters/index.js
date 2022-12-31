2cca894724286973143ad06d901233135f78e7ef
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 11:57:03 +0100

    Fixing a bug

--- 1-js_intro/5. Build a Chrome Extension/46. Arguments vs Parameters/index.js
+++ 1-js_intro/5. Build a Chrome Extension/46. Arguments vs Parameters/index.js
@@ -1,5 +1,5 @@
-// What are greeting and name? pa
-// What are "Howdy" and "James"?
+// What are greeting and name? parameters
+// What are "Howdy" and "James"? 
 // What are num1 and num2?
 // What are 3 and 4?
 
