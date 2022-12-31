e60e77788f56ea0bf1c7658615ed1503b16a0553
Hayats <hayatudeenabdulrahman@gmail.com>
2022-12-31 19:15:56 +0100

    Updating the documentation

--- 2-js_web-projects/1. quote-generator/script.js
+++ 2-js_web-projects/1. quote-generator/script.js
@@ -4,7 +4,9 @@ let apiQuotes = []
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
-        const response = await fetch(apiUrl)
-        apiQuotes = await response.json()
-    } catch 
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+    } catch (error) {
+        // Catch Error Here
+    }
 }
