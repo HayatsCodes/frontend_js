1aa84e8cf418fe212d8eaab133c06611ae666333
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-01 14:01:22 +0100

    Fixing a bug

--- 2-js_web-projects/1. quote-generator/script.js
+++ 2-js_web-projects/1. quote-generator/script.js
@@ -1,6 +1,7 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = []
