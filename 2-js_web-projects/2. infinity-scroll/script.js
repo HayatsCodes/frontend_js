1395e38db27e9ce4ef5659248594b6466e938706
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-02 12:14:18 +0100

    Updating the code

--- 2-js_web-projects/2. infinity-scroll/script.js
+++ 2-js_web-projects/2. infinity-scroll/script.js
@@ -11,7 +11,7 @@ const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count
 // Function to set attributes
 function setAttributes(element, attributes) {
     for (const key in attributes) {
-        element.setAttribute(key, attributes[key])
+        element.setAttribute(key, attributes.key)
     }
 }
 
@@ -21,14 +21,15 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+
 
         // Create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
 
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
