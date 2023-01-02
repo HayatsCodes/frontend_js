a88120814475748f4ea34bfe5fbe47315d57f6d3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-02 13:02:34 +0100

    Refactoring the code

--- 2-js_web-projects/2. infinity-scroll/script.js
+++ 2-js_web-projects/2. infinity-scroll/script.js
@@ -1,6 +1,9 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
