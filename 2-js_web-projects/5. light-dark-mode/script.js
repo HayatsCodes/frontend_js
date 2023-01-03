586d09121cee37b690a2c4252acace39bd813e63
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 22:54:26 +0100

    Updating the code

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -1,6 +1,6 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
-const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
 const nav = document.getElementById('nav');
 const nav = document.getElementById('nav');
 const nav = document.getElementById('nav');
