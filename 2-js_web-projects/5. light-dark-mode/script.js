ea60c97a1b4841c9fc8834cedaa1c4406ff602d7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 22:56:27 +0100

    Refactoring the code

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -4,7 +4,7 @@ const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
-const nav = document.getElementById('nav');
+const textBox = document.getElementById('text-box');
 
 // Switch Theme Dynamically
 function switchTheme(event) {
