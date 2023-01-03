08afcb7eacce2f895e8e0d5e639c9d1131ae6bbe
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 22:55:27 +0100

    Updating the documentation

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -1,9 +1,9 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const nav = document.getElementById('nav');
-const nav = document.getElementById('nav');
-const nav = document.getElementById('nav');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
 const nav = document.getElementById('nav');
 
 // Switch Theme Dynamically
