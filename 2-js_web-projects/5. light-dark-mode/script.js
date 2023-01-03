1374fa9e1997986dfb83bd948dc8aea8e638c516
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 23:29:55 +0100

    Updating the code

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -51,4 +51,8 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
 }
