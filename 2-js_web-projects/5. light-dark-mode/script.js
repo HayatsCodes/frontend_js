57bfb7af94adc639fe989b9af9c5ec7c860c10ac
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 22:29:57 +0100

    Fixing a bug

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -2,7 +2,11 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-    
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        
+    }
 }
 
 toggleSwitch.addEventListener('change', switchTheme)
