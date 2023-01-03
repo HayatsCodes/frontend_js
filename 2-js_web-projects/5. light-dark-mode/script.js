45c97e5081be88b3ed549ce234ee68545f36c55f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-03 22:28:56 +0100

    Improving performance

--- 2-js_web-projects/5. light-dark-mode/script.js
+++ 2-js_web-projects/5. light-dark-mode/script.js
@@ -2,7 +2,7 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-
+    
 }
 
 toggleSwitch.addEventListener('change', switchTheme)
