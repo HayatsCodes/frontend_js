bc4df0a3e95deda62b5dba7b01bc660a04abb0aa
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-06 09:30:36 +0100

    Fixing a bug

--- 2-js_web-projects/8-quran_player/script.js
+++ 2-js_web-projects/8-quran_player/script.js
@@ -3,6 +3,8 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+// Check If Playing
+
 // Play
 function playQuran() {
     quran.play();
