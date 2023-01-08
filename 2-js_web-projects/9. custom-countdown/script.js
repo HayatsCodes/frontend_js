17cd92cd7fdfcbfff2c9cae10bd4493d20e25fbe
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 07:26:56 +0100

    Refactoring the code

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -2,9 +2,13 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let coun
+
 // set Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute('min', today);
+
 // Take Values from Form Input
 function updateCountdown(e) {
     
