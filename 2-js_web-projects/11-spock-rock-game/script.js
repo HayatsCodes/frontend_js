863e958e9c2e46cf92ce043c2054836205d35140
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 17:27:04 +0100

    Fixing a bug

--- 2-js_web-projects/11-spock-rock-game/script.js
+++ 2-js_web-projects/11-spock-rock-game/script.js
@@ -8,7 +8,15 @@ const playerRock = document.getElementById('playerRock');
 const playerPaper = document.getElementById('playerPaper');
 const playerScissors = document.getElementById('playerScissors');
 const playerLizard = document.getElementById('playerLizard');
-const playerRock = document.getElementById('playerRock');
+const playerSpock = document.getElementById('playerRock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerRock');
+
+
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
