2bf88a13fc52db85f97b8a3c451587e7229e3b2d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 08:40:25 +0100

    Refactoring the code

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -32,7 +32,6 @@ function updateDom() {
 
     // Populate Countdown
     countdownElTitle.textContent = `${countdownTitle}`;
-    // console.log( 'countdown', countdownTitle);
     timeElements[0].textContent = `${days}`;
     // console.log( 'span-0', timeElements[0]);
     timeElements[1].textContent = `${hours}`;
