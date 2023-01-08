bef873ccfdbd2f137344d120e9297fb7d4c9388f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 09:03:08 +0100

    Adding a new feature

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -10,6 +10,7 @@ const timeElements = document.querySelectorAll('span');
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
+let c
 
 const second = 1000;
 const minute = second * 60;
