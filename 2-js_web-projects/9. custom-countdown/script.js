d0576482af69035cee3c532f10d350681218add9
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 07:15:18 +0100

    Refactoring the code

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -1,3 +1,3 @@
 const inputContainer = document.getElementById('input-container');
-const inputContainer = document.getElementById('input-container');
+const coun = document.getElementById('input-container');
 const inputContainer = document.getElementById('input-container');
