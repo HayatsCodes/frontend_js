53a4d216d40b80436fc6a4339800368e6c59e597
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 09:06:10 +0100

    Updating the code

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -23,7 +23,24 @@ dateEl.setAttribute('min', today);
 
 // Populate Countdown / Complete UI
 function updateDom() {
-    countdownActive = setInterval(() =)
+    countdownActive = setInterval(() => {
+        const now = new Date().getTime();
+    const distance = countdownValue - now;
+
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+
+    // Populate Countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
+    inputContainer.hidden = true;
+    countdownEl.hidden = false;
+    }, second);
 }
 
 // Take Values from Fminute Input
