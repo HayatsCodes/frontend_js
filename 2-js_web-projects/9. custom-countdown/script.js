22f4c45abaf0e77c318118185e86f8ae861fe30d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 07:20:20 +0100

    Updating the documentation

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -4,3 +4,4 @@ const dateEl = document.getElementById('date-picker');
 
 // set Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute('min', today);
