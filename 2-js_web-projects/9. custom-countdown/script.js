d5bb1ef85948c98925c5ad78421a016c1d552716
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-08 07:44:41 +0100

    Improving performance

--- 2-js_web-projects/9. custom-countdown/script.js
+++ 2-js_web-projects/9. custom-countdown/script.js
@@ -2,6 +2,8 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+
+
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
