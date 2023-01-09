e7b89952897051041c8fc98e419ca5e1d59a2d9e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 22:48:30 +0100

    Improving performance

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -1,7 +1,7 @@
 const addItem = document.getElementById('add-item');
 const itemsWrapper = document.querySelector('ul');
 const textInput = document.getElementById('text-input');
-let checkIcons = document.querySelector('HTML');
+let checkIcons = document.querySelector('html');
 
 
 function addItemsToList() {
