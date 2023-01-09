47d0628512c99b0c590594e5b88c727b3c951ee3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 16:48:59 +0100

    Updating the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -6,48 +6,27 @@ isElementInItemsWrapper = itemsWrapper.hasChildNodes();
 
 function addItemsToList() {
     if (textInput.value) {
-      // Create a new li element
-      const li = document.createElement('li');
-      li.classList.add('items-list');
-  
-      // Create the span elements for the text and icons
-      const textSpan = document.createElement('span');
-      textSpan.textContent = textInput.value;
-      const iconsSpan = document.createElement('span');
-  
-      // Create the icons and append them to the icons span
-      const checkIcon = document.createElement('i');
-      checkIcon.classList.add('fas', 'fa-check');
-      const penIcon = document.createElement('i');
-      penIcon.classList.add('fas', 'fa-pen');
-      const trashIcon = document.createElement('i');
-      trashIcon.classList.add('fas', 'fa-trash');
-      iconsSpan.appendChild(checkIcon);
-      iconsSpan.appendChild(penIcon);
-      iconsSpan.appendChild(trashIcon);
-  
-      // Append the text and icons spans to the li element
-      li.appendChild(textSpan);
-      li.appendChild(iconsSpan);
-  
-      // Append the li element to the ul element
-      itemsWrapper.appendChild(li);
-  
-      textInput.value = '';
+        itemsWrapper.innerHTML += ` <li class="items-list">
+        <span>${textInput.value}</span>
+        <span> <i class="fas fa-check"></i>
+            <i class="fas fa-pen"></i>
+            <i class="fas fa-trash"></i>
+        </span>
+    </li>`
+        textInput.value = '';
+        isElementInItemsWrapper = true;
     }
-  }
-  
- 
-if (isElementInItemsWrapper) {
-    const checkIcons = document.querySelectorAll('.fa-check');
-    checkIcons.forEach(checkIcon => {
-        checkIcon.addEventListener('click', event => {
-            const li = event.target.parentNode.parentNode;
-            console.log(li);
-            li.classLists.toggle('completed')
-        });
-    });
 }
+
+const checkIcons = document.querySelectorAll('.fa-check');
+checkIcons.forEach(checkIcon => {
+    checkIcon.addEventListener('click', event => {
+        const li = event.target.parentNode.parentNode;
+        console.log(li);
+        li.classLists.toggle('completed')
+    });
+});
+
 
 // Add Events Listener
 addItem.addEventListener('click', addItemsToList);
