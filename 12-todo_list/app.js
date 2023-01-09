adc72b591114b70a5ea83679394435da21816592
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 15:48:38 +0100

    Fixing a bug

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -5,15 +5,39 @@ const textInput = document.getElementById('text-input');
 isElementInItemsWrapper = itemsWrapper.hasChildNodes();
 
 function addItemsToList() {
-    if (textInput.value) {
-        itemsWrapper.innerHTML += ` <li class="items-list">
-        <span>${textInput.value}</span>
-        <span> <i class="fas fa-check"></i>
-            <i class="fas fa-pen"></i>
-            <i class="fas fa-trash"></i>
-        </span>
-    </li>`
-        textInput.value = '';
+    function addItemsToList() {
+        if (textInput.value) {
+          // Create a new li element
+          const li = document.createElement('li');
+          li.classList.add('items-list');
+      
+          // Create the span elements for the text and icons
+          const textSpan = document.createElement('span');
+          textSpan.textContent = textInput.value;
+          const iconsSpan = document.createElement('span');
+      
+          // Create the icons and append them to the icons span
+          const checkIcon = document.createElement('i');
+          checkIcon.classList.add('fas', 'fa-check');
+          const penIcon = document.createElement('i');
+          penIcon.classList.add('fas', 'fa-pen');
+          const trashIcon = document.createElement('i');
+          trashIcon.classList.add('fas', 'fa-trash');
+          iconsSpan.appendChild(checkIcon);
+          iconsSpan.appendChild(penIcon);
+          iconsSpan.appendChild(trashIcon);
+      
+          // Append the text and icons spans to the li element
+          li.appendChild(textSpan);
+          li.appendChild(iconsSpan);
+      
+          // Append the li element to the ul element
+          itemsWrapper.appendChild(li);
+      
+          textInput.value = '';
+        }
+      }
+      
         isElementInItemsWrapper = true;
     }
 }
