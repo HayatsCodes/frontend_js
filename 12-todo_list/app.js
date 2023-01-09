1c1fd257519d1f9580df7e9eace32d491001fa85
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 22:47:30 +0100

    Refactoring the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -1,7 +1,7 @@
 const addItem = document.getElementById('add-item');
 const itemsWrapper = document.querySelector('ul');
 const textInput = document.getElementById('text-input');
-let checkIcons = document.querySelector('ul');
+let checkIcons = document.querySelector('HTML');
 
 
 function addItemsToList() {
