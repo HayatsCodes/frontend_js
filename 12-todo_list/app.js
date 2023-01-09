4b91cd5476d0479a00e53953ec06aebfeb0dd194
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 23:57:45 +0100

    Updating the documentation

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -19,14 +19,6 @@ function addItemsToList() {
 
 
 
-function markItemAsChecked(e) {
-    console.log('checkIcons', checkIcons);
-    checkIcons.forEach(() => {
-            const textSpan = e.target.parentNode.previousElementSibling;
-            console.log(textSpan);
-            textSpan.style.textDecoration = 'line-through red 3px';
-        });
-    };
 
 
 
