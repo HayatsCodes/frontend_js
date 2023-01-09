7f7d29cb081c822c29fe84ba0dd903616590c188
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 14:41:32 +0100

    Updating the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -1,9 +1,9 @@
 const addItem = document.getElementById('add-item');
-const itemsWrapper = document.getElementById('items-wrapper');
+const itemsWrapper = document.querySelector('ul');
 const textInput = document.getElementById('text-input');
 
 function addItemsToList() {
-
+    
 }
 
 // Add Events Listener
