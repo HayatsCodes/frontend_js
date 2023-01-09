87b51e16a42e308062e0658dfa7059bd1db6ff69
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-09 15:26:17 +0100

    Updating the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -3,20 +3,21 @@ const itemsWrapper = document.querySelector('ul');
 const textInput = document.getElementById('text-input');
 
 function checkItem() {
-    
+
+    const checkIcon = document.querySelectorAll('.fa-check');
 }
 
 
 function addItemsToList() {
     if (textInput.value) {
-        itemsWrapper.innerHTML +=` <li class="items-list">
+        itemsWrapper.innerHTML += ` <li class="items-list">
         <span>${textInput.value}</span>
         <span> <i class="fas fa-check"></i>
             <i class="fas fa-pen"></i>
             <i class="fas fa-trash"></i>
         </span>
     </li>`
-    textInput.value = '';
+        textInput.value = '';
     }
 }
 
