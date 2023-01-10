119a7e34bfe9e99ce7190e6ed930f52e295531ab
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-10 22:44:00 +0100

    Updating the documentation

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -51,13 +51,8 @@ function editItem(e) {
     textSpan.setAttribute("onkeypress","return (this.innerText.length <= 35)");
     textSpan.setAttribute("contenteditable", "true");
     textSpan.focus();
-    textSpan.addEventListener('blur', (e) => {
-        if (textSpan.innerText.length <= 1) {
-            e.preventDefault();
-            textSpan.textDecoration = 'underline red 3px'
-        } else {
-            textSpan.setAttribute("contenteditable", "false");
-        }
+    textSpan.addEventListener('blur', () => {
+        textSpan.setAttribute("contenteditable", "false");
     });
 }
 
