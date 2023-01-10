55918b0f758ac5c43502908fc0db313e3d327c3b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-11 00:37:09 +0100

    Fixing a bug

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -26,7 +26,7 @@ function iconsEventListener() {
 function addItemsToList() {
     if (textInput.value) {
         if (itemsWrapper.length <= 8) {
-            console.log(itemsWrapper.length)
+            console.log(itemsWrapper.length);
             itemsWrapper.innerHTML += ` <li class="items-list">
         <span contenteditable="false">${textInput.value}</span>
         <span> <i class="fas fa-check"></i>
