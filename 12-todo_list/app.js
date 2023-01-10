d12c24d2c53bf55e8dc1a8a91346a55882850cdd
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-10 23:53:45 +0100

    Updating the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -3,7 +3,7 @@ const itemsWrapper = document.querySelector('ul');
 const textInput = document.getElementById('text-input');
 
 let isChecked = false;
-let isFocus = false;
+let isFocused = false;
 
 // Icons Function
 
@@ -31,6 +31,7 @@ function addItemsToList() {
     </li>`
         textInput.value = '';
         iconsEventListener();
+
     }
 }
 
@@ -47,19 +48,24 @@ function markItemAsChecked(e) {
 }
 
 function editItem(e) {
-    const textSpan = e.target.parentNode.previousElementSibling;
-    textSpan.setAttribute("onkeypress","return (this.innerText.length <= 35)");
-    textSpan.setAttribute("contenteditable", "true");
-    textSpan.focus();
-    textSpan.addEventListener('blur', (event) => {
-        if (textSpan.innerText.length < 1) {
-            event.preventDefault();
-            textSpan.focus();
-            textSpan.style.textDecoration = 'underline red 3px'
-        } else {
-            textSpan.setAttribute("contenteditable", "false");
-        }
-    });
+    if (!isChecked) {
+        isFocused = true;
+        const textSpan = e.target.parentNode.previousElementSibling;
+        textSpan.setAttribute("onkeypress", "return (this.innerText.length <= 35)");
+        textSpan.setAttribute("contenteditable", "true");
+        textSpan.focus();
+        textSpan.style.outline = " 0px solid black";
+        textSpan.addEventListener('blur', (event) => {
+            if (textSpan.innerText.length < 1) {
+                event.preventDefault();
+                textSpan.focus();
+                textSpan.style.caretColor = 'red';
+            } else {
+                textSpan.setAttribute("contenteditable", "false");
+                textSpan.style.caretColor = 'black';
+            }
+        });
+    }
 }
 
 // Add Events Listener
