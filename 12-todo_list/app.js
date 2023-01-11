3d499b4579e1bc033e69ab9f82b1eb635e424f0b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-12 00:28:33 +0100

    Updating the code

--- 12-todo_list/app.js
+++ 12-todo_list/app.js
@@ -39,7 +39,7 @@ function addItemsToList() {
             iconsEventListener();
             localStorage.setItem('toDoItems', JSON.stringify((itemsWrapper.innerHTML)));
             localStorage.setItem('listsInItemsWrapper', JSON.stringify((listsInItemsWrapper)));
-        } else {
+        } else if{
             p = document.createElement("p");
             p.innerText = "Items Limit Reached!";
             itemsWrapper.append(p);
