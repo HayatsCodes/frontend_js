05019893c8ecddc6a8dc9b3ac71c91e044216627
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-15 10:28:02 +0100

    Updating the documentation

--- 3-advanced_js/bind_call_apply.js
+++ 3-advanced_js/bind_call_apply.js
@@ -1,7 +1,8 @@
-// Call: it's a method that every function uses when they're invoked
+// Call: 1. it's a method that every function uses when they're invoked
 function a() {
     console.log('a');
 }
 
 console.log(a.call() == a());
 
+//  Call: 2. it can also be use dto call  
