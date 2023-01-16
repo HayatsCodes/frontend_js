0317ebd5a0b83375f00016c1b3274d1768e2fd6a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-16 14:37:27 +0100

    Adding a new feature

--- 3-advanced_js/pillars.js
+++ 3-advanced_js/pillars.js
@@ -1,3 +1,4 @@
 // Functions are special type of object that is callable
 // Functions are first class citizens in JS:
-// 1. 
+// 1. They can be assigned to a variable
+// 2. They can be passed as a parameter to another function
