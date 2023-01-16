e3c001be53d62eb4af23ce9e13cef50b0eb2d9bd
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-16 14:39:28 +0100

    Refactoring the code

--- 3-advanced_js/pillars.js
+++ 3-advanced_js/pillars.js
@@ -1,5 +1,9 @@
 // Functions are special type of object that is callable
 // Functions are first class citizens in JS:
 // 1. They can be assigned to a variable
+var stuff = function() {}
 // 2. They can be passed as a parameter to another function
-// 3. They can be returned 
+function a(fn) {
+    fn()
+}
+// 3. They can be returned in a function
