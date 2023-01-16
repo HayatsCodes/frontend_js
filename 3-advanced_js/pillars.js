763c8d47a7f6fad7896fd7e5e6ae1504f7afb640
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-16 14:41:29 +0100

    Fixing a bug

--- 3-advanced_js/pillars.js
+++ 3-advanced_js/pillars.js
@@ -2,12 +2,14 @@
 // Functions are first class citizens in JS:
 // 1. They can be assigned to a variable
 var stuff = function() {}
-// 2. They can be passed as a parameter to another function
+// 2. They can be passed to another function
 function a(fn) {
     fn();
 }
 a(function() {console.log('hi there')});
 // 3. They can be returned in a function
 function b() {
-    return
+    return function c() {console.log('bye')}
 }
+var d = b();
+d()
