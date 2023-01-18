bbe1f62569044907b6b2edfbae41f3ae28fd196a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-18 17:46:08 +0100

    Updating the code

--- 3-advanced_js/prototypal_inheritance.js
+++ 3-advanced_js/prototypal_inheritance.js
@@ -1,27 +1,27 @@
 let animal = {
     eats: true,
-  };
-  let rabbit = {
+};
+let rabbit = {
     jumps: true
-  };
-  
-  rabbit.__proto__ = animal; // (*)
-  
-  // we can find both properties in rabbit now:
-  console.log( rabbit.eats ); // true
-  console.log( rabbit.jumps ); // true
+};
 
-  animal = {
+rabbit.__proto__ = animal; // (*)
+
+// we can find both properties in rabbit now:
+console.log(rabbit.eats); // true
+console.log(rabbit.jumps); // true
+
+animal = {
     eats: true,
     walk() {
         console.log("Animal walk");
     }
-  };
-  
-  let rabbit = {
+};
+
+rabbit = {
     jumps: true,
     __proto__: animal
-  };
-  
-  // walk is taken from the prototype
-  rabbit.walk(); // Animal walk
+};
+
+// walk is taken from the prototype
+rabbit.walk(); // Animal walk
