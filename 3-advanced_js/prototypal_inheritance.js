2796f685143f689ef71a6a2ab9e1be442b6a7573
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-18 17:45:07 +0100

    Updating the documentation

--- 3-advanced_js/prototypal_inheritance.js
+++ 3-advanced_js/prototypal_inheritance.js
@@ -11,4 +11,17 @@ let animal = {
   console.log( rabbit.eats ); // true
   console.log( rabbit.jumps ); // true
 
+  animal = {
+    eats: true,
+    walk() {
+        console.log("Animal walk");
+    }
+  };
   
+  let rabbit = {
+    jumps: true,
+    __proto__: animal
+  };
+  
+  // walk is taken from the prototype
+  rabbit.walk(); // Animal walk
