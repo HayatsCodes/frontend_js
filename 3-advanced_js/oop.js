eec579865e3d87c13b2a6ddffcd40e79690c8ed5
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-19 11:22:07 +0100

    Fixing a bug

--- 3-advanced_js/oop.js
+++ 3-advanced_js/oop.js
@@ -21,16 +21,16 @@
 //     }
 // }
 
-const elfMethod = {
-    attack() {
-        return 'attack with ' + this.weapon;
-    }
-}
+// const elfMethod = {
+//     attack() {
+//         return 'attack with ' + this.weapon;
+//     }
+// }
 
-function createElf(name, weapon) {
-    let newElf = Object.create(elfMethod);
-    newElf.name = name;
-    newElf.weapon = weapon;
-    return newElf;
-}
+// function createElf(name, weapon) {
+//     let newElf = Object.create(elfMethod);
+//     newElf.name = name;
+//     newElf.weapon = weapon;
+//     return newElf;
+// }
 
