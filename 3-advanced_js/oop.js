c4d891c3afba63c8d0bcefb3a9269b283d7947bc
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-19 10:43:03 +0100

    Refactoring the code

--- 3-advanced_js/oop.js
+++ 3-advanced_js/oop.js
@@ -11,4 +11,12 @@
 // }
 
 // Factory Functions
-function createElf(name, weapon)
+function createElf(name, weapon) {
+    return {
+        name: name,
+        weapon: weapon,
+        attack() {
+            return 'attack with ' + weapon
+        }
+    }
+}
