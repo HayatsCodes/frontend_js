5276991ffb04dc280bffe7fafc0b036205a8f9f0
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-20 17:52:21 +0100

    Adding a new feature

--- 3-advanced_js/fp.js
+++ 3-advanced_js/fp.js
@@ -1,25 +1,25 @@
-const user = {
-    name: 'Kim',
-    active: true,
-    cart: [],
-    purchases: []
-}
+// const user = {
+//     name: 'Kim',
+//     active: true,
+//     cart: [],
+//     purchases: []
+// }
 
-function addItemsToCart(items) {
-    user.cart.push(...items);
-}
+// function addItemsToCart(items) {
+//     user.cart.push(...items);
+// }
 
-function taxItems(cart) {
-    for (let i = 0; i < cart.length; i++) {
-        cart[i].price *= 1.03;
-    }
-}
+// function taxItems(cart) {
+//     for (let i = 0; i < cart.length; i++) {
+//         cart[i].price *= 1.03;
+//     }
+// }
 
-function BuyItem(cart) {
-    user.purchases.push(...cart);
-    emptyCart();
-}
+// function BuyItem(cart) {
+//     user.purchases.push(...cart);
+//     emptyCart();
+// }
 
-function emptyCart() {
-    user.cart = [];
-}
+// function emptyCart() {
+//     user.cart = [];
+// }
