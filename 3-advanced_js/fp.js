610349e157a928cc9df4cb58a0df92701f7873e8
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-20 17:59:28 +0100

    Fixing a bug

--- 3-advanced_js/fp.js
+++ 3-advanced_js/fp.js
@@ -32,5 +32,9 @@
 const array = [1, 2, 3]
 function removeLastItem(arr) {
     const newArray = [].concat(arr);
-    newArray.pop()
+    newArray.pop();
+    return newArray;
+}
+function multiplyBy2(arr) {
+    return arr.map()
 }
