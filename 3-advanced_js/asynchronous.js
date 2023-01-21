7be88cdf7df69709c3b36cb68260a1fc58b60da8
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-21 14:33:45 +0100

    Adding a new feature

--- 3-advanced_js/asynchronous.js
+++ 3-advanced_js/asynchronous.js
@@ -1,10 +1,14 @@
 // Memory heap:
 // Where memory is allocated for states
 
-
+// Memory leak:
+// When there is too much unused states in a program
 
 // Call stack:
 // where our code is executed
+
+// Stack overflow:
+// 
 
 // Single threaded:
 // Has one call stacks and call stack is called once at a time
