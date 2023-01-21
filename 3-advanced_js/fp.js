2d4a3f6e6cec591c04255a76048f288a3a4687fb
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-21 07:47:17 +0100

    Fixing a bug

--- 3-advanced_js/fp.js
+++ 3-advanced_js/fp.js
@@ -1,28 +1,3 @@
-// const user = {
-//     name: 'Kim',
-//     active: true,
-//     cart: [],
-//     purchases: []
-// }
-
-// function addItemsToCart(items) {
-//     user.cart.push(...items);
-// }
-
-// function taxItems(cart) {
-//     for (let i = 0; i < cart.length; i++) {
-//         cart[i].price *= 1.03;
-//     }
-// }
-
-// function BuyItem(cart) {
-//     user.purchases.push(...cart);
-//     emptyCart();
-// }
-
-// function emptyCart() {
-//     user.cart = [];
-// }
 
 // the rules of Pure Functions:
 // 1. The same input should always produce the same output
