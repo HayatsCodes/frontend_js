5f546e780224a068abddca1fc4fea6e4e4ef4b0b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-21 22:27:10 +0100

    Improving performance

--- 3-advanced_js/modules.js
+++ 3-advanced_js/modules.js
@@ -1,3 +1,4 @@
 // Module Pattern:
-// It uses encapsulation, closure and IIFE to store data in a function, so that data can only be accessed by the function scope
+// It uses encapsulation, closure and IIFE to store data in a function, 
+// so that data can only be accessed by the function scope
 
