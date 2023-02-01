56275a97c286bf2d1f0c53abcd01cee50fa6b716
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-01 23:04:09 +0100

    Improving performance

--- 4-nodejs/fundamentals/hello.js
+++ 4-nodejs/fundamentals/hello.js
@@ -3,5 +3,5 @@ const mission = 'learn';
 if (mission === 'learn') {
     console.log('Time to write some Node code!');
 } else {
-    console.log('Is ' + mission + ' really ')
+    console.log('Is ' + mission + ' really more fun');
 }
