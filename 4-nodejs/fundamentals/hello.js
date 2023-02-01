8798aec48f3f5a1e5ac9cc7ee1446c62edd78333
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-01 23:03:09 +0100

    Updating the documentation

--- 4-nodejs/fundamentals/hello.js
+++ 4-nodejs/fundamentals/hello.js
@@ -3,5 +3,5 @@ const mission = 'learn';
 if (mission === 'learn') {
     console.log('Time to write some Node code!');
 } else {
-    console.log('Is ' + mission + )
+    console.log('Is ' + mission + ' really ')
 }
